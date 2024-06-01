a98395664bacc2ae6f3c7035d5a5fad6be761265
schance14 <schance.sd14@gmail.com>
2024-05-31 19:27:19 -0600

    feat: Creation of movie-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 
 @Component({
@@ -24,4 +25,13 @@ openUserLoginDialog(): void {
 // Assigning the dialog a width
   width: '280px'
 });
-}}
+}
+
+openMoviesDialog(): void {
+  this.dialog.open(MovieCardComponent, {
+    width: '500px'
+  });
+}
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 
 @NgModule({
@@ -24,6 +26,8 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
+    MovieCardComponent,
+    WelcomePageComponent,
   ],
   imports: [
     BrowserModule,
