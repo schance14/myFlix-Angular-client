bee41e67e4733ddfd3422f6f5ac4ba6132e8c55e
schance14 <schance.sd14@gmail.com>
2024-06-04 19:37:01 -0600

    feat: create a welcome page component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -9,6 +9,7 @@ import { UserRegistrationService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -22,7 +23,8 @@ export class UserLoginFormComponent implements OnInit {
 constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar, 
+    private router: Router) { }
 
 ngOnInit(): void {
 }
@@ -37,6 +39,7 @@ userLogin(): void {
      this.snackBar.open(result, 'OK', {
         duration: 2000
      });
+     this.router.navigate(['movies'])
     }, (result) => {
       this.snackBar.open(result, 'OK', {
         duration: 2000
