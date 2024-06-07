832d89d6604d0931252918dd53eff7ffcfa98b94
schance14 <schance.sd14@gmail.com>
2024-06-07 14:05:12 -0600

    feat: add movie card functionality

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { UserRegistrationService } from '../fetch-api-data.service'
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { DirectorInfoComponent } from '../director-info/director-info.component';
+import { GenreInfoComponent } from '../genre-info/genre-info.component';
+import { SynopsisInfoComponent } from '../synopsis-info/synopsis-info.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -7,9 +13,13 @@ import { UserRegistrationService } from '../fetch-api-data.service'
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: UserRegistrationService) { }
+  user: any = {};
+  favoritesList: any[] = [];
+
+  constructor(public fetchApiData: UserRegistrationService, public dialog: MatDialog, public snackBar: MatSnackBar,
+    public router: Router) { }
 
 ngOnInit(): void {
   this.getMovies();
@@ -22,4 +32,66 @@ getMovies(): void {
       return this.movies;
     });
   }
+
+  openGenre(name: string, description: string): void {
+  this.dialog.open(GenreInfoComponent, {
+    data: {
+      Name: name,
+      Description: description
+    }
+  })
+}
+
+openDirector(name: string, description: string, birth: string, death: string): void {
+  this.dialog.open(DirectorInfoComponent, {
+    data: {
+      Name: name,
+      Description: description,
+      Birth: birth,
+      Death: death
+    }
+  })
+}
+
+openSynopsis(title: string, description: string): void{
+  this.dialog.open(SynopsisInfoComponent, {
+    data: {
+     Title: title,
+     Description: description
+    }
+  })
+}
+
+isFavorite(movie: any): boolean {
+  const favoriteMovie = this.favoritesList.filter((title) => title === movie.Title);
+  return favoriteMovie.length ? true : false;
+}
+
+addMovieToFavorites(movie: any): void{
+  this.fetchApiData.addFavoriteMovies().subscribe((resp: any) => {
+    console.log(resp);
+    const user = JSON.parse(localStorage.getItem('currentUser') || '');
+    user.addMovieToFavorites.push(movie.Title);
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    this.favoritesList.push(movie.Title);
+      this.snackBar.open('Movie added to your list!', 'Okay', {
+        duration: 2000,
+      });
+  })
+}
+
+deleteMovieFromFavorites(movie: any): void {
+  this.fetchApiData.deleteFavoriteMovie().subscribe((resp: any) => {
+    console.log(resp);
+    const user = JSON.parse(localStorage.getItem('currentUser') || '');
+    user.addMovieToFavorites = user.addMovieToFavorites.filter(
+      (title: string) => title !== movie.Title);
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.favoritesList = this.favoritesList.filter((title: string) => title !== movie.Title);
+      this.snackBar.open('Movie removed from list!', 'Okay', {
+        duration: 2000,
+      });
+  })
+}
+
 }
